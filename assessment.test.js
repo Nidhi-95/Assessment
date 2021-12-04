6599338e66066060671342fdd97132459bf65345
Nidhi-95 <nidhirathore1895@gmail.com>
2021-12-04 14:00:51 +1030

    Fixed tests

--- assessment.test.js
+++ assessment.test.js
@@ -6,7 +6,7 @@ test('adds 1 + 2 to equal 3', () => {
 });
 
 test('adds 2 + 3 to equal 5', () => {
-  expect(add(2, 3)).toBe(6);
+  expect(add(2, 3)).toBe(5);
 });
 
 
@@ -14,8 +14,8 @@ test('subtract 5 - 2 to equal 3', () => {
   expect(subtract(5, 2)).toBe(3);
 });
 
-test('subtract 10 - 8 to equal 3', () => {
-  expect(subtract(10, 8)).toBe(3);
+test('subtract 10 - 8 to equal 2', () => {
+  expect(subtract(10, 8)).toBe(2);
 });
 
 test('product 5 * 4 to equal 20', () => {
